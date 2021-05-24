02581e096afc845d9af3bb09825b29caea0dd441
PKPenguin321 <pkpenguin321.git@gmail.com>
2021-05-23 17:59:50 -0700

    version checking + rtf update

--- MusicHax/MusicHax.cs
+++ MusicHax/MusicHax.cs
@@ -175,5 +175,10 @@ namespace MusicHax
                 return null;
             }
         }
+
+        private const string modVersion = "v2.0";
+        private const string repositoryOwner = "PKPenguin321";
+        private const string repositoryName = "LLBMusicHax";
+
     }
 }
