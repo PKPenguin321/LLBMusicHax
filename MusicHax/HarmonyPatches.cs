d494a3078a816cf0eec44de0d7520bfd920f00a2
Glomzubuk <glomzubuk@gmail.com>
2021-08-31 21:24:52 +0200

    Almost full rewrite again to use bepinex, harmony and LLBML

---
+++ MusicHax/HarmonyPatches.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using LLHandlers;
+
+namespace MusicHax
+{
+    public static class PlayMusicPatch
+    {
+        [HarmonyPatch(typeof(LLHandlers.AudioHandler), nameof(LLHandlers.AudioHandler.PlayMusic), typeof(AudioClip), typeof(Vector2))]
+        [HarmonyPrefix]
+        public static bool PlayMusic_Prefix(ref AudioClip clip)
+        {
+            MusicHax.CreateMusicDirectory(clip.name);
+            AudioClip newClip = MusicHax.GetAudioClipFor(clip.name);
+            if(newClip != null)
+            {
+                clip = newClip;
+            }
+            return true;
+        }
+    }
+}
--- MusicHax/MusicHax.cs
+++ MusicHax/MusicHax.cs
@@ -1,164 +1,82 @@
 ﻿using System;
 using System.IO;
 using UnityEngine;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
 using LLHandlers;
-using LLScreen;
+using LLBML;
+using LLBML.Audio;
 
 namespace MusicHax
 {
-    public class MusicHax : MonoBehaviour
+    [BepInPlugin(PluginInfos.PLUGIN_ID, PluginInfos.PLUGIN_NAME, PluginInfos.PLUGIN_VERSION)]
+    [BepInDependency("fr.glomzubuk.plugins.llb.llbml", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("no.mrgentle.plugins.llb.modmenu", BepInDependency.DependencyFlags.SoftDependency)]
+    [BepInProcess("LLBlaze.exe")]
+    public class MusicHax : BaseUnityPlugin
     {
-        #region modinfos
-        public const string modVersion = "v2.1";
-        public const string repositoryOwner = "PKPenguin321";
-        public const string repositoryName = "LLBMusicHax";
-        #endregion
-
-        public static MusicHax instance = null;
-        private static ModMenuIntegration MMI = null;
-        private static readonly string MHResourcesPath = Path.Combine(Path.Combine(Application.dataPath, "Managed"), "MusicHaxResources");
-
+        //public static readonly string MHResourcesPath = Path.Combine(Path.Combine(Path.Combine(Application.dataPath, "Managed"), "MusicHaxResources");
+        public static readonly string MHResourcesPath = Path.Combine(BepInEx.Paths.ManagedPath, "MusicHaxResources");
         private static AudioCache musicCache = new AudioCache();
-        private static AudioCache sfxCache = new AudioCache();
-        private static AudioCache voiceCache = new AudioCache();
 
+        private static ConfigEntry<bool> enablePreloading;
+        private static ConfigEntry<bool> enableVanillaMusics;
 
-        public static void Initialize()
+        private readonly string loadingText = "MusicHax is loading External Songs...";
+
+        // Awake is called once when both the game libs and the plug-in are loaded
+        void Awake()
         {
-            GameObject gameObject = new GameObject("MusicHax");
-            instance = gameObject.AddComponent<MusicHax>();
-            DontDestroyOnLoad(gameObject);
+            Logger.LogInfo("Hello, world!");
+
+            var harmoInstance = new Harmony(PluginInfos.PLUGIN_ID);
+            Logger.LogDebug("Patching AudioHandler");
+            harmoInstance.PatchAll(typeof(PlayMusicPatch));
+
+            Directory.CreateDirectory(MHResourcesPath);
+
+            enablePreloading = this.Config.Bind<bool>("Toggles", "EnablePreloading", false);
+            enableVanillaMusics = this.Config.Bind<bool>("Toggles", "EnableVanillaMusics", false);
         }
 
-        #region configs
-        private static bool PreloadingEnabled
+        void Start()
         {
-            get
+            Logger.LogInfo("Searching ModMenu");
+            if (BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("no.mrgentle.plugins.llb.modmenu"))
             {
-                if (MMI != null)
-                    return MMI.GetTrueFalse(MMI.configBools["(bool)enablePreloading"]);
-                return true;
+                Logger.LogInfo("Registering to ModMenu");
+                ModMenu.ModMenu.RegisterMod(this.Info);
+            }
+
+            if (enablePreloading.Value)
+            {
+                LoadingScreen.SetLoading(this.Info, true, loadingText);
+                LoadMusics();
+                LoadingScreen.SetLoading(this.Info, false); //TODO doesn' t work, this is async
             }
         }
 
-        private static bool VanillaMusicEnabled
-        {
-            get
-            {
-                if (MMI != null)
-                    return MMI.GetTrueFalse(MMI.configBools["(bool)allowVanillaMusicToRandomlyPlay"]);
-                return true;
-            }
-        }
-        #endregion
-
-        private bool loadingExternalMusicFiles = false;
-        private bool didLoad = false;
-        private const string loadingText = "MusicHax is loading External Sounds...";
 
         void Update()
         {
-            if (MMI == null) { MMI = gameObject.AddComponent<ModMenuIntegration>(); }
-            else
-            {
-                if (PreloadingEnabled)
-                {
-                    if (this.didLoad == false)
-                    {
-                        loadingExternalMusicFiles = true;
-                        didLoad = true;
-                        LoadMusics();
-                        LoadSfx();
-                        LoadVoice();
-                    } else if (!musicCache.IsLoading && !sfxCache.IsLoading && !voiceCache.IsLoading)
-                    {
-                        UIScreen.SetLoadingScreen(false);
-                        loadingExternalMusicFiles = false;
-                    }
-                }
-            }
-
             if (DNPFJHMAIBP.AKGAOAEJ != null && DNPFJHMAIBP.AKGAOAEJ.musicAssetLinks != null)
             {
                 CreateMusicDirectories();
             }
         }
 
-        private void OnGUI()
-        {
-            var OriginalColor = GUI.contentColor;
-            var OriginalLabelFontSize = GUI.skin.label.fontSize;
-            var OriginalLabelAlignment = GUI.skin.label.alignment;
-
-            GUI.contentColor = Color.white;
-            GUI.skin.label.fontSize = 50;
-            if (UIScreen.loadingScreenActive && this.loadingExternalMusicFiles == true)
-            {
-                GUIStyle label = new GUIStyle(GUI.skin.label);
-                var sX = Screen.width / 2;
-                var sY = UIScreen.GetResolutionFromConfig().height / 3;
-                GUI.skin.label.alignment = TextAnchor.MiddleCenter;
-                GUI.Label(new Rect(0, sY-25, Screen.width, sY-75), loadingText);
-                GUI.skin.label.alignment = TextAnchor.MiddleLeft;
-            }
-            GUI.contentColor = OriginalColor;
-            GUI.skin.label.fontSize = OriginalLabelFontSize;
-            GUI.skin.label.alignment = OriginalLabelAlignment;
-
-        }
-
         private void LoadMusics()
         {
-            UIScreen.SetLoadingScreen(true, false, false, Stage.NONE);
-
             musicCache.Clear();
 
-            DirectoryInfo[] musicDirectories = Directory.CreateDirectory(MHResourcesPath).GetDirectories();
+            DirectoryInfo[] musicDirectories = new DirectoryInfo(MHResourcesPath).GetDirectories();
             foreach (DirectoryInfo musicDirectory in musicDirectories)
             {
                 foreach (AudioInfo musicInfo in AudioUtils.GetAudioInfos(musicDirectory))
                 {
-                    Debug.Log("MusicHax: Loading new " + musicDirectory.Name + " : " + musicInfo.file.FullName);
+                    Logger.LogDebug("Loading new " + musicDirectory.Name + " : " + musicInfo.file.FullName);
                     musicCache.LoadClip(musicDirectory.Name, musicInfo, Path.GetFileNameWithoutExtension(musicInfo.file.FullName));
-                }
-            }
-        }
-
-        private void LoadSfx()
-        {
-            sfxCache.Clear();
-
-            DirectoryInfo[] sfxDirectories = Directory.CreateDirectory(Path.Combine(MHResourcesPath, "Sfx")).GetDirectories();
-            foreach (DirectoryInfo sfxDirectory in sfxDirectories)
-            {
-                foreach (AudioInfo sfxInfo in AudioUtils.GetAudioInfos(sfxDirectory))
-                {
-                    Debug.Log("Loading new " + sfxDirectory.Name + " : " + sfxInfo.file.FullName);
-                    sfxCache.LoadClip(sfxDirectory.Name, sfxInfo);
-                }
-            }
-        }
-
-        private static string GetVoiceCacheKey(string audioFile, Character character)
-        {
-            return character.ToString() + "_" + audioFile;
-        }
-        private void LoadVoice()
-        {
-            voiceCache.Clear();
-
-            DirectoryInfo[] charDirectories = Directory.CreateDirectory(Path.Combine(MHResourcesPath, "Voice")).GetDirectories();
-            foreach (DirectoryInfo charDirectory in charDirectories)
-            {
-                foreach (DirectoryInfo voiceDirectory in charDirectory.GetDirectories())
-                {
-                    foreach (AudioInfo voiceInfo in AudioUtils.GetAudioInfos(voiceDirectory))
-                    {
-                        Debug.Log("Loading new " + voiceDirectory.Name + " : " + voiceInfo.file.FullName);
-
-                        string cacheKey = GetVoiceCacheKey(voiceDirectory.Name, GetCharacterByName(charDirectory.Name));
-                        voiceCache.LoadClip(cacheKey, voiceInfo);
-                    }
                 }
             }
         }
@@ -181,28 +99,18 @@ namespace MusicHax
             {
                 if (i == 13) continue;
                 string directoryname = DNPFJHMAIBP.AKGAOAEJ.musicAssetLinks.GetMusicAsset((AudioTrack)i).audioClipName;
-                Debug.Log("Creating directory: " + directoryname);
+                this.Logger.LogInfo("Creating directory: " + directoryname);
                 Directory.CreateDirectory(Path.Combine(MHResourcesPath, directoryname));
             }
             directoryAlreadyCreated = true;
         }
 
-        public static AudioClip GetMusicClip(AudioClip inputClip)
+        public static AudioClip GetAudioClipFor(string clipName)
         {
-            string clipName = inputClip.name;
             Debug.Log("MusicHax: Got asked for a clip named: \"" + clipName + "\"");
-            int lowerBound = 0;
-            if (VanillaMusicEnabled) { lowerBound = -1; }
-            if (PreloadingEnabled && musicCache.ContainsKey(clipName) && musicCache[clipName].Count > 0)
+            if (enablePreloading.Value && musicCache.ContainsKey(clipName) && musicCache[clipName].Count > 0)
             {
-                int roll = UnityEngine.Random.Range(lowerBound, musicCache[clipName].Count);
-                Debug.Log("MusicHax: PRELOADED. VanillaMusicEnabled = " + VanillaMusicEnabled + ", lowerBound = " + lowerBound + ", roll = " + roll);
-                if (roll == -1)
-                {
-                    Debug.Log("Returning input clip");
-                    return inputClip;
-                }
-                return musicCache[clipName][roll];
+                return musicCache[clipName][UnityEngine.Random.Range(0, musicCache[clipName].Count)];
             }
             else
             {
@@ -211,58 +119,16 @@ namespace MusicHax
                     string[] pathList = Directory.GetFiles(Path.Combine(MHResourcesPath, clipName));
                     if (pathList.Length > 0)
                     {
-                        int roll = UnityEngine.Random.Range(lowerBound, pathList.Length - 1);
-                        Debug.Log("MusicHax: NOPRELOAD. VanillaMusicEnabled = " + VanillaMusicEnabled + ", lowerBound = " + lowerBound + ", roll = " + roll);
-                        if (roll == -1) { return inputClip; }
-                        return AudioUtils.GetClipSynchronously(pathList[roll]);
+                        return AudioUtils.GetClipSynchronously(pathList[UnityEngine.Random.Range(0, pathList.Length - 1)]);
                     }
-                } catch (Exception e)
+                }
+                catch (Exception e)
                 {
                     Debug.LogError("MusicHax: Exception caught: ");
                     Debug.LogException(e);
                 }
                 return null;
             }
-        }
-
-        public static AudioClip[] GetSfxClips(string audioFile)
-        {
-            Debug.Log("MusicHax: Got asked for a sfx clip named: \"" + audioFile + "\"");
-
-            Directory.CreateDirectory(Path.Combine(Path.Combine(MHResourcesPath, "Sfx"), audioFile));
-
-            if (sfxCache.ContainsKey(audioFile) && sfxCache[audioFile].Count > 0)
-            {
-                return sfxCache[audioFile].ToArray();
-            }
-            return new AudioClip[0];
-        }
-
-        public static AudioClip[] GetVoiceClips(string audioFile, Character character)
-        {
-            Debug.Log("MusicHax: Got asked for a voice clip named: \"" + audioFile + "\" for " + character.ToString());
-
-            Directory.CreateDirectory(Path.Combine(Path.Combine(Path.Combine(MHResourcesPath, "Voice"), character.ToString()), audioFile));
-
-            string cacheKey = GetVoiceCacheKey(audioFile, character);
-            if (voiceCache.ContainsKey(cacheKey) && voiceCache[cacheKey].Count > 0)
-            {
-                return voiceCache[cacheKey].ToArray();
-            }
-            return new AudioClip[0];
-        }
-
-        public static Character GetCharacterByName(string characterName)
-        {
-            for (int i = 0; i < (int)Character._MAX_NORMAL; i++)
-            {
-                Character currentCharacter = (Character)i;
-                if (currentCharacter.ToString() == characterName)
-                {
-                    return currentCharacter;
-                }
-            }
-            return Character.NONE;
         }
     }
 }
